ed41b052aba3c27a94bd165c7373bb8d78f4236c
Perkles <adrianegonaves@gmail.com>
2022-03-18 20:26:00 -0300

    Enviando do exercício 5 até o exercíco 15

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -61,7 +61,6 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
     let maiorNumero
     let menorNumero
 
-
     if (num1 > num2) {
         maiorNumero = num1;
         menorNumero = num2;
@@ -83,50 +82,150 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+
+    let arrayPares = [];
+
+    for(let i = 0; arrayPares.length < n; i++) { console.log(i, arrayPares.length)
+        if(i % 2===0 ) {
+            arrayPares.push(i);
+        }
+    }
+    return arrayPares
+ 
    
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
+    let tresLadosIguais = "Equilátero"
+    let  doisLadosIguais = "Isósceles"
+    let tresLadosDiferentes = "Escaleno"
 
+    if( ladoA === ladoB && ladoC ===ladoA && ladoB === ladoC){
+        return tresLadosIguais
+
+    }else if ( ladoA === ladoB || ladoC ===ladoA || ladoB === ladoC) {
+        return doisLadosIguais
+
+    }else if (ladoA !== ladoB && ladoC !==ladoA && ladoB !== ladoC){
+        return tresLadosDiferentes
+    }
+    return 'erro'
+    
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
+    const ordenaNumeros = array.sort(function( a,b){
+        if (a >b ) return 1;
+        if (a < b) return -1;
+        return 0;
+    })
+    return [ordenaNumeros[ordenaNumeros.length-2], ordenaNumeros[1]]
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(", ")}.`
    
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    const copiaPessoa ={
+        ... pessoa,
+        nome:'ANÔNIMO'
+    }
+    return copiaPessoa
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
+    const alturaPermitida = 1.5;
+    const idadeMininaPermitida = 14;
+    const idadeMaximaPermitida = 60;
+
+    const pessoasAutorizadas = pessoas.filter ((pessoa) =>{
+        if ( pessoa.altura > alturaPermitida && pessoa.idade >idadeMininaPermitida && pessoa.idade < idadeMaximaPermitida) {
+            return true
+        }
+        return false
+    })
+    return pessoasAutorizadas
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    const alturaPermitida = 1.5;
+    const idadeMininaPermitida = 14;
+    const idadeMaximaPermitida = 60;
+
+    const pessoasNaoAutorizadas = pessoas.filter ((pessoa) =>{
+        if ( pessoa.altura > alturaPermitida && pessoa.idade >idadeMininaPermitida && pessoa.idade < idadeMaximaPermitida) {
+            return false
+        }
+        return true
+        
+    })
+    return pessoasNaoAutorizadas
+ 
 }
 
 // EXERCÍCIO 14
+function somaItensArray (array){
+    let somaDosItens = 0 
+    for (let i=0; i<array.length; i++){
+        somaDosItens += array[i];
+    }
+
+    return somaDosItens
+}
+
 function retornaContasComSaldoAtualizado(contas) {
+
+    let novoSaldoTatal = 0
+    let soma = 0
+
+    for (let i=0; i<contas.length; i++){ 
+        soma = somaItensArray (contas[i].compras)
+        novoSaldoTatal = contas[i].saldoTotal - soma
+
+        contas[i].saldoTotal = novoSaldoTatal
+        contas[i].compras = []
+    }
+    return contas
+
 
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
+      return consultas.sort(function (a,b) {
+        if (a.nome >b.nome) return 1;
+        if (a.nome <b.nome) return -1;
+        return 0;
+    })
   
 }
 
 // EXERCÍCIO 15B
+// função auxilar para converter data no formato strgin para formato data 
+function converterData (data) {
+    let dia = parseInt(data.split("/")[0]);
+    let mes = parseInt(data.split("/")[1]);
+    let ano = parseInt(data.split("/")[2]);
+
+    return new Date ( ano, mes-1, dia)
+}
 function retornaArrayOrdenadoPorData(consultas) {
+
+    return consultas.sort(function (a,b) {
+        return (converterData(a.dataDaConsulta) - converterData(b.dataDaConsulta))
+      
+    })
    
 }
+
